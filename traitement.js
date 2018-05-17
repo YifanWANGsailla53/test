5f733d690318b5b24d65fbff6ed3d321aa38f595
Yifan WANG <ytwangyifan4@gmail.com>
2018-05-17 20:34:04 +0200

    Update traitement.js

--- traitement.js
+++ traitement.js
@@ -14,15 +14,20 @@ refreshGPS();
 var destEmail=localStorage.getItem('destEmail');
 var destSMS=localStorage.getItem('destSMS');
 var latitude,longitude,precision,miseAjour;//variables globales pour ne pas avoir à les récupèrer à chaque fois dans la DOM
-document.getElementById('destEmail').innerHTML=destEmail;
-document.getElementById('destSMS').innerHTML=destSMS;
+var sujet;
+var message;
+var connection=(window.navigator.onLine?'online':'offline');
+console.log('connection:'+connection);
+document.getElementById('connection').innerHTML=connection;
+document.getElementById('destEmail').value=destEmail;
+document.getElementById('destSMS').value=destSMS;
 
 function refreshGPS(){
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-} else {
-  window.alert('Votre navigateur ne supporte pas la géolocalisation');
-}
+	if ('geolocation' in navigator) {
+		navigator.geolocation.getCurrentPosition(success, error, options);
+	} else {
+	  window.alert('Votre navigateur ne supporte pas la géolocalisation');
+	}
 }
 
 var options = {
@@ -32,6 +37,11 @@ var options = {
 };
 
 function success(pos) {
+	//mise à jour de l'état de connection
+	connection=(window.navigator.onLine?'online':'offline');
+	console.log('connection:'+connection);
+	document.getElementById('connection').innerHTML=connection;
+	//mise à jour des coordonnées
 	var coord = pos.coords;
 	latitude=coord.latitude.toPrecision(6);
 	longitude=coord.longitude.toPrecision(6);
@@ -42,7 +52,20 @@ function success(pos) {
 	document.getElementById("longitude").innerHTML=longitude;
 	document.getElementById("precision").innerHTML=precision;
 	document.getElementById("miseAjour").innerHTML=miseAjour;
-	console.log("table actualisée");
+	console.log("Coordonnées mises à jour");
+	//mise à jour des variables globales
+	sujet='Geolocalisation '+d.getHours()+"h"+d.getMinutes();
+	message="Mes coordonnées mesurées par l'appli Web Progressive\r\nLatitude: "+latitude+'\r\nLongitude: '+longitude+'\r\nPrécision: '+precision+'\r\nDernière mise à jour: '+miseAjour;
+	console.log("Variables globales mises à jour");
+	//mise à jour du fichier téléchargeable
+	var d=new Date();
+	var blob = new Blob([message], {type: 'text/plain',charset:'utf-8'});
+	var url=window.URL.createObjectURL(blob);
+	console.log('Blob '+blob+' créee');
+	var lien=document.getElementById("telecharger");
+	lien.setAttribute('href',url);
+	lien.setAttribute('download',sujet+".txt");
+	console.log('Lien de téléchargement mis à jour');
 }
 
 function error(err) {
@@ -50,24 +73,29 @@ function error(err) {
 }
 
 function email(){
-	window.alert(document.getElementById('destEmail').innerHTML);
-	if(document.getElementById('destEmail').innerHTML!=destEmail){
-		console.log("actualisation du destinataire Email");
-		destEmail=document.getElementById('destEmail').innerHTML;
+	if(document.getElementById('destEmail').value!=destEmail){
+		destEmail=document.getElementById('destEmail').value;
 		localStorage.setItem('destEmail',destEmail);
+		console.log("Destinataire Email enregistré");
 	}
 	var subject='mes coordonnees';
-	var message="Mes coordonnées mesurées par l'appli Web Progressive<br>"
-	+'Latitude: '+latitude+'<br>Longitude: '+longitude+'<br>Précision: '+precision+'<br>Dernière mise à jour: '+miseAjour;
-	console.log("envoi d'un Email");
-	var email='mailto:'+destEmail+'?subject='+subject+'?body='+body;
-	confirmation=window.confirm(email);
-	if(confirmation==true){window.location.href=email;}
+	var ecMessage=encodeURIComponent(message);
+	var email='mailto:'+destEmail+'?subject='+subject+'&body='+ecMessage;
+	// confirmation=window.confirm(email);
+	// if(confirmation==true){
+	window.location.href=email;
+	console.log("Email ouvert dans l'application de messagerie");
 }
+
 function sms(){
-	console.log("envoi d'un SMS");
-	navigator.mozSms.send(number, message);
-}
-function facebook(){
-	console.log("envoi d'une alerte Facebook");
+	if(document.getElementById('destSMS').value!=destSMS){
+		console.log("Destinataire SMS enregistré");
+		destSMS=document.getElementById('destSMS').value;
+		localStorage.setItem('destSMS',destSMS);
+	}
+	var ecMessage=encodeURIComponent(message);
+	//var sms='sms://'+destSMS+'?body='+ecmessage;//& pour iOS
+	var sms='sms://0649624189?body=message';//& pour iOS
+	window.location.href=sms;
+	console.log("SMS ouvert dans l'application SMS");
 }
